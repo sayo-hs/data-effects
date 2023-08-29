b13c03142e32676ae2bb17d68bef7cbc179e7b6a
Yamada Ryo <ymdfield@outlook.jp>
2023-08-29 13:59:33 +0900

    [add] Send/SendF classes.

--- src/Control/Effect/Class.hs
+++ src/Control/Effect/Class.hs
@@ -6,8 +6,8 @@ module Control.Effect.Class where
 
 import Data.Kind (Type)
 
-type Instruction = Type -> Type
 type Signature = (Type -> Type) -> (Type -> Type)
+type Instruction = Type -> Type
 
 {- | Lift an /instruction/ (a datatype of first-order effect) to a /signature/
     (a datatype of higher-order effect).
@@ -15,5 +15,11 @@ type Signature = (Type -> Type) -> (Type -> Type)
      Come from [heft-lang\/POPL2023\/haskell\/src\/Elab.hs]
     (https://github.com/heft-lang/POPL2023/blob/74afe1d5ce0b491cffe40cc5c73a2a5ee6a94d9c/haskell/src/Elab.hs#L9-L10).
 -}
-newtype LiftIns ins (f :: Type -> Type) (a :: Type) = LiftIns {unliftIns :: ins a}
+newtype LiftIns (ins :: Instruction) (f :: Type -> Type) a = LiftIns {unliftIns :: ins a}
     deriving stock (Functor, Foldable, Traversable)
+
+class Send (sig :: Signature) m where
+    send :: sig m a -> m a
+
+class SendF (ins :: Instruction) m where
+    sendF :: ins a -> m a
