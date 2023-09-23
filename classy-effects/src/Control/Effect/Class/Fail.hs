907fe9b47c71fd9ab49ea4b1d0a463244e29f50a
Yamada Ryo <ymdfield@outlook.jp>
2023-09-23 13:19:19 +0900

    [add] some effects from the base and mtl.

---
+++ classy-effects/src/Control/Effect/Class/Fail.hs
@@ -0,0 +1,10 @@
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this
+-- file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+module Control.Effect.Class.Fail where
+
+class Fail f where
+    fail :: String -> f a
+
+makeEffectF ''Fail
--- classy-effects/src/Prelude.hs
+++ classy-effects/src/Prelude.hs
@@ -4,8 +4,8 @@
 -- License, v. 2.0. If a copy of the MPL was not distributed with this
 -- file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-module Prelude (module Prelude, makeEffect, makeEffectF, makeEffectH, Type) where
+module Prelude (module Prelude, makeEffect, makeEffectF, makeEffectH, makeEmptyEffect, Type) where
 
-import Control.Effect.Class.Machinery.TH (makeEffect, makeEffectF, makeEffectH)
+import Control.Effect.Class.Machinery.TH (makeEffect, makeEffectF, makeEffectH, makeEmptyEffect)
 import Data.Kind (Type)
 import "base" Prelude
