830c46652636ea6d06bca3590b766821515ad052
Yamada Ryo <ymdfield@outlook.jp>
2024-10-09 17:01:17 +0900

    [fix] Redefine Provider as a type of Unlift

--- data-effects/src/Data/Effect/Unlift/Provider.hs
+++ data-effects/src/Data/Effect/Unlift/Provider.hs
@@ -14,43 +14,43 @@ Portability :  portable
 This module provides the `Provider` effect, like [@Effectful.Provider@](https://hackage.haskell.org/package/effectful-core-2.3.0.0/docs/Effectful-Provider.html)
 in the @effectful@ package.
 -}
-module Data.Effect.Provider where
+module Data.Effect.Unlift.Provider where
 
 import Data.Effect.Key (type (##>))
 import Data.Functor.Identity (Identity, runIdentity)
 
-data Provider' ctx i hdls b (f :: Type -> Type) (a :: Type) where
+data Provider' ctx i b (f :: Type -> Type) (a :: Type) where
     Provide
         :: i
-        -> (forall s. hdls s b -> (forall x. f x -> b x) -> b a)
-        -> Provider' ctx i hdls b f (ctx a)
+        -> ((forall x. f x -> b x) -> b a)
+        -> Provider' ctx i b f (ctx a)
 makeEffectH [''Provider']
 
-data ProviderKey ctx i hdls
+data ProviderKey ctx i
 
-type Provider ctx i hdls b = ProviderKey ctx i hdls ##> Provider' ctx i hdls b
-type Provider_ i hdls b = ProviderKey Identity i hdls ##> Provider' Identity i hdls b
+type Provider ctx i b = ProviderKey ctx i ##> Provider' ctx i b
+type Provider_ i b = ProviderKey Identity i ##> Provider' Identity i b
 
 infix 2 .!
 
 (.!)
-    :: forall i hdls f a b
-     . ( SendHOEBy (ProviderKey Identity i hdls) (Provider' Identity i hdls b) f
+    :: forall i f a b
+     . ( SendHOEBy (ProviderKey Identity i) (Provider' Identity i b) f
        , Functor f
        )
     => i
-    -> (forall s. hdls s b -> (f ~> b) -> b a)
+    -> ((f ~> b) -> b a)
     -> f a
-i .! f = runIdentity <$> provide'' @(ProviderKey Identity i hdls) i f
+i .! f = runIdentity <$> provide'' @(ProviderKey Identity i) i f
 {-# INLINE (.!) #-}
 
 infix 2 ..!
 
 (..!)
-    :: forall ctx i hdls f a b
-     . (SendHOEBy (ProviderKey ctx i hdls) (Provider' ctx i hdls b) f)
+    :: forall ctx i f a b
+     . (SendHOEBy (ProviderKey ctx i) (Provider' ctx i b) f)
     => i
-    -> (forall s. hdls s b -> (f ~> b) -> b a)
+    -> ((f ~> b) -> b a)
     -> f (ctx a)
-i ..! f = provide'' @(ProviderKey ctx i hdls) i f
+i ..! f = provide'' @(ProviderKey ctx i) i f
 {-# INLINE (..!) #-}
