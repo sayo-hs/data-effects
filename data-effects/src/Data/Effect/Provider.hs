48425e364068b5321c8afeee072cdc4d339c7c81
Yamada Ryo <ymdfield@outlook.jp>
2024-11-03 01:08:52 +0900

    [fix] Generalize 'Provider' w.r.t. `ctx` result wrapper.

--- data-effects/src/Data/Effect/Provider.hs
+++ data-effects/src/Data/Effect/Provider.hs
@@ -20,11 +20,11 @@ import Data.Functor.Identity (Identity, runIdentity)
 
 -- | An effect to introduce a new local scope that provides effect context @b@.
 data Provider' ctx i b (f :: Type -> Type) (a :: Type) where
-    -- | Introduces a new local scope that provides an effect context @b p@ parameterized by type @i p@ and with results wrapped in @ctx@.
+    -- | Introduces a new local scope that provides an effect context @b p@ parameterized by type @i p@ and with results wrapped in @ctx p@.
     Provide
         :: i p
         -> ((forall x. f x -> b p x) -> b p a)
-        -> Provider' ctx i b f (ctx a)
+        -> Provider' ctx i b f (ctx p a)
 
 makeEffectH [''Provider']
 
@@ -38,7 +38,7 @@ type Provider ctx i b = ProviderKey ctx i ##> Provider' ctx i b
 An effect to introduce a new local scope that provides effect context @b@.
 A version of `Provider` where the result is not wrapped in a specific container.
 -}
-type Provider_ i b = ProviderKey Identity i ##> Provider' Identity (Const i :: () -> Type) (Const1 b)
+type Provider_ i b = Provider (Const1 Identity) (Const i :: () -> Type) (Const1 b)
 
 newtype Const1 f x a = Const1 {getConst1 :: f a}
 
@@ -50,8 +50,8 @@ A version of `..!` where the result is not wrapped in a specific container.
 (.!)
     :: forall i f a b
      . ( SendHOEBy
-            (ProviderKey Identity (Const i :: () -> Type))
-            (Provider' Identity (Const i) (Const1 b))
+            (ProviderKey (Const1 Identity :: () -> Type -> Type) (Const i :: () -> Type))
+            (Provider' (Const1 Identity) (Const i) (Const1 b))
             f
        , Functor f
        )
@@ -59,19 +59,20 @@ A version of `..!` where the result is not wrapped in a specific container.
     -> ((f ~> b) -> b a)
     -> f a
 i .! f =
-    runIdentity <$> provide'' @(ProviderKey Identity (Const i :: () -> Type))
-        (Const i)
-        \run -> Const1 $ f $ getConst1 . run
+    runIdentity . getConst1
+        <$> provide'' @(ProviderKey (Const1 Identity :: () -> _ -> _) (Const i :: () -> _))
+            (Const i)
+            \run -> Const1 $ f $ getConst1 . run
 {-# INLINE (.!) #-}
 
 infix 2 ..!
 
--- | A operator to introduce a new local scope that provides effect context @b@.
+-- | A operator to introduce a new local scope that provides effect context @b p@.
 (..!)
     :: forall ctx i p f a b
      . (SendHOEBy (ProviderKey ctx i) (Provider' ctx i b) f)
     => i p
     -> ((f ~> b p) -> b p a)
-    -> f (ctx a)
+    -> f (ctx p a)
 i ..! f = provide'' @(ProviderKey ctx i) i f
 {-# INLINE (..!) #-}
