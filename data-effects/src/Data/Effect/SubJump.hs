2444e55fed01cb011c506fda69436767d093eb11
Sayo <ymdfield@outlook.jp>
2024-11-08 16:05:58 +0900

    [add] Sub/Jump effects for continuation.

---
+++ data-effects/src/Data/Effect/SubJump.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+
+module Data.Effect.SubJump where
+
+import Data.Function (fix)
+
+data SubJump' ref a where
+    SubFork :: SubJump' ref (Either (ref a) a)
+    Jump :: ref a -> a -> SubJump' ref b
+
+makeKeyedEffect [''SubJump'] []
+
+sub :: (SendFOEBy SubJumpKey (SubJump' ref) m, Monad m) => (ref a -> m b) -> (a -> m b) -> m b
+sub p q = subFork >>= either p q
+{-# INLINE sub #-}
+
+callCC :: (SendFOEBy SubJumpKey (SubJump' ref) m, Monad m) => ((a -> m b) -> m a) -> m a
+callCC f = sub (f . jump) pure
+{-# INLINE callCC #-}
+
+getCC :: (SendFOEBy SubJumpKey (SubJump' ref) m, Monad m) => m (m a)
+getCC = callCC $ pure . fix
+{-# INLINE getCC #-}
