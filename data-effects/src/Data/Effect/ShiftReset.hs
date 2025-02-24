7f44f33abbabd2ae4e339b604b52791403e153f9
Sayo <ymdfield@outlook.jp>
2025-02-25 01:59:19 +0900

    improve the interface of ShiftReset.

--- data-effects/src/Data/Effect/ShiftReset.hs
+++ data-effects/src/Data/Effect/ShiftReset.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE QuantifiedConstraints #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 -- SPDX-License-Identifier: MPL-2.0
 
@@ -6,39 +8,65 @@ module Data.Effect.ShiftReset where
 
 import Control.Monad ((>=>))
 
-data Shift (ans :: Type) b :: Effect where
-    Shift
+data UnliftShift (ans :: Type) b :: Effect where
+    UnliftShift
         :: forall ans b m a
          . ((a -> b ans) -> (forall x. m x -> b x) -> b ans)
-        -> Shift ans b m a
-makeEffectH ''Shift
+        -> UnliftShift ans b m a
+makeEffectH' (def & noGenerateLabel) ''UnliftShift
+
+data UnliftShiftLabel (ans :: Type)
+type instance LabelOf (UnliftShift ans b) = UnliftShiftLabel ans
+
+type Shift ff ans es = UnliftShift ans (ShiftC ff ans es)
+
+newtype ShiftC ff ans es a
+    = ShiftC {unShiftC :: Eff ff (Shift ff ans es ': es) a}
+
+shift
+    :: forall a ans es es' ff c
+     . (Shift ff ans es' :> es, forall f. (c (ff f)) => Monad (ff f), Free c ff)
+    => ( (a -> Eff ff (Shift ff ans es' ': es') ans)
+         -> (Eff ff es ~> Eff ff (Shift ff ans es' ': es'))
+         -> Eff ff (Shift ff ans es' ': es') ans
+       )
+    -> Eff ff es a
+shift initiate =
+    unliftShift \k run ->
+        ShiftC $ initiate (unShiftC . k) (unShiftC . run)
+{-# INLINE shift #-}
 
 callCC
-    :: forall a ans b es ff c
-     . (Shift ans b :> es, Monad (Eff ff es), Monad b, Free c ff)
-    => ((a -> b ans) -> Eff ff es a)
+    :: forall a ans es es' ff c
+     . (Shift ff ans es' :> es, forall f. (c (ff f)) => Monad (ff f), Free c ff)
+    => ((a -> Eff ff (Shift ff ans es' ': es') ans) -> Eff ff es a)
     -> Eff ff es a
 callCC f = shift \k run -> run (f $ k >=> run . abort) >>= k
 {-# INLINE callCC #-}
 
 abort
-    :: forall a ans b es ff c
-     . (Shift ans b :> es, Applicative b, Free c ff)
+    :: forall a ans es es' ff c
+     . (Shift ff ans es' :> es, Applicative (Eff ff (Shift ff ans es' ': es')), Free c ff)
     => ans
     -> Eff ff es a
-abort ans = shift \_ _ -> pure ans
+abort ans = unliftShift \_ _ -> ShiftC $ pure ans
 {-# INLINE abort #-}
 
 getCC
-    :: forall ans b es ff c
-     . (Shift ans b :> es, Monad (Eff ff es), Monad b, Free c ff)
-    => Eff ff es (b ans)
+    :: forall ans es es' ff c
+     . (Shift ff ans es' :> es, forall f. (c (ff f)) => Monad (ff f), Free c ff)
+    => Eff ff es (Eff ff (Shift ff ans es' ': es') ans)
 getCC = callCC \exit' -> let a = exit' a in pure a
+{-# INLINE getCC #-}
 
-embed :: forall ans b es ff c. (Shift ans b :> es, Monad b, Free c ff) => b ~> Eff ff es
-embed m = shift \k _ -> m >>= k
-{-# INLINE embed #-}
+detachShift
+    :: forall ans a es es' ff c
+     . (Shift ff ans es' :> es, Monad (Eff ff (Shift ff ans es' ': es')), Free c ff)
+    => Eff ff (Shift ff ans es' ': es') a
+    -> Eff ff es a
+detachShift m = unliftShift \k _ -> ShiftC $ m >>= unShiftC . k
+{-# INLINE detachShift #-}
 
-data Reset m (a :: Type) where
+data Reset :: Effect where
     Reset :: m a -> Reset m a
 makeEffectH ''Reset
