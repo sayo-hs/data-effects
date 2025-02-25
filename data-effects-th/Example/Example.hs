a8842a2485d6bec3b1b8ec7b431b26c8054d0f6d
Sayo <ymdfield@outlook.jp>
2025-02-25 15:06:04 +0900

    drop support for GHC 9.2, 9.4.

--- data-effects-th/Example/Example.hs
+++ data-effects-th/Example/Example.hs
@@ -1,39 +1,39 @@
-{-# LANGUAGE AllowAmbiguousTypes #-}
-{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 -- SPDX-License-Identifier: MPL-2.0
 
 module Example where
 
+import Data.Effect (Effect)
 import Data.Effect.HFunctor (HFunctor)
 import Data.Effect.HFunctor.TH (makeHFunctor, makeHFunctor')
-import Data.Effect.TH (makeEffect, makeEffectH)
+import Data.Effect.TH (makeEffectF, makeEffectH)
 import Data.Kind (Type)
 import Data.List.Infinite (Infinite ((:<)))
 
-data Throw e (a :: Type) where
-    Throw :: e -> Throw e a
+data Throw e :: Effect where
+    Throw :: e -> Throw e f a
 
-data Catch e f (a :: Type) where
+data Catch e :: Effect where
     Catch :: f a -> (e -> f a) -> Catch e f a
 
-makeEffect [''Throw] [''Catch]
+makeEffectF ''Throw
+makeEffectH ''Catch
 
-data Unlift b f (a :: Type) where
+data Unlift b :: Effect where
     WithRunInBase :: ((forall x. f x -> b x) -> b a) -> Unlift b f a
 
-makeEffectH [''Unlift]
+makeEffectH ''Unlift
 
-data Nested (f :: Type -> Type) (a :: Type) where
+data Nested :: Effect where
     Nested :: ([f a -> Int] -> Int) -> Nested f a
 makeHFunctor ''Nested
 
-data ManuallyCxt (g :: Type -> Type) h (f :: Type -> Type) (a :: Type) where
+data ManuallyCxt (g :: Type -> Type) h :: Effect where
     ManuallyCxt :: g (h f a) -> ManuallyCxt g h f a
 makeHFunctor' ''ManuallyCxt \(g :< h :< _) -> [t|(Functor $g, HFunctor $h)|]
 
-data NestedTuple (f :: Type -> Type) (a :: Type) where
+data NestedTuple :: Effect where
     NestedTuple :: ((forall x. (f x, f a) -> Int) -> Int) -> NestedTuple f a
 makeHFunctor ''NestedTuple
 
